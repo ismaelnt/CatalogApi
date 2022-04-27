4685facc72265c1fa76954df68b8826569fa16f0
ismaelnt <ismaelnt97@gmail.com>
2022-04-27 12:46:55 -0300

    feat: category model

---
+++ CatalogApi/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CatalogApi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+}
